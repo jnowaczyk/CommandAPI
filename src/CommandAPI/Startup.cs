85a557ec2565ca48f59c22bfb9afc87e40f1feee
jnowaczyk <jacek.nowaczyk@gmail.com>
2020-11-28 12:07:14 +0100

    API: Startup class has access to IConfiguration. IConfiguration used to fetch connectionString from appsettings.json

--- src/CommandAPI/Startup.cs
+++ src/CommandAPI/Startup.cs
@@ -1,6 +1,8 @@
 using CommandAPI.Data;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
@@ -8,13 +10,27 @@ namespace CommandAPI
 {
     public class Startup
     {
+        // IConfiguration daje dostep do "configuration API" 
+        // to pozwala uzyskac dostęp do konfiguracji trzymanych m.in. w appsettings.json
+        // 
+
+        public IConfiguration Configuration { get; }
+
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+
+
+
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
             services.AddScoped<ICommandAPIRepo,MockCommandAPIRepo>();
-
+            services.AddDbContext<CommandContext>( options => options.UseNpgsql(Configuration.GetConnectionString("PostgreSqlConnection")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
